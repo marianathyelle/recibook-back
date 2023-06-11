8ae15f2a816ea2907997f0ebd44782c69dc5fff2
Maria <nathyellemonteiro19@gmail.com>
2023-06-11 10:34:39 -0500

    init

---
+++ src/main/java/com/recibook/recibookserver/RecibookApplication.java
@@ -0,0 +1,13 @@
+package com.recibook.recibookserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecibookApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecibookApplication.class, args);
+	}
+
+}
